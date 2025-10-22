3fcffa0964449f97b492376d015f78ab7ce8bc0d
gazi04 <gazmendhalili2016@gmail.com>
2025-10-22 15:50:51 +0200

    :sparkles: Added new docker services for RabbitMQ and Celery
    This commit add a simple celery app with the required service

---
+++ data_pipeline/celery_app.py
@@ -0,0 +1,15 @@
+from celery import Celery
+from core.config_loader import settings
+
+app = Celery("data_task",
+             broker=settings.celery_broker_url,
+             backend=settings.celery_result_backend)
+
+# Optional configuration, see the application user guide.
+app.conf.update(
+    result_expires=3600,
+)
+
+if __name__ == "__main__":
+    app.start()
+
