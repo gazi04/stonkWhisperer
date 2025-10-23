238c0f295c9ca783fcfa1b8bd7ed44097f3a2302
gazi04 <gazmendhalili2016@gmail.com>
2025-10-23 14:59:49 +0200

    :sparkels: Implemented Celery for better performance in fetching data with trafilatura and insertind data into the database

--- data_pipeline/celery_app.py
+++ data_pipeline/celery_app.py
@@ -3,7 +3,8 @@ from core.config_loader import settings
 
 app = Celery("data_task",
              broker=settings.celery_broker_url,
-             backend=settings.celery_result_backend)
+             backend=settings.celery_result_backend,
+             include=["tasks.extraction", "tasks.loading"])
 
 # Optional configuration, see the application user guide.
 app.conf.update(
--- data_pipeline/tasks/loading.py
+++ data_pipeline/tasks/loading.py
@@ -1,17 +1,76 @@
 from prefect import task
+from sqlalchemy import select
+from typing import Any, Dict, List
+
+from celery_app import app
 from core.database import get_db
 from models.article import Article
+
 import pandas as pd
 
+
+@task(name="Dispatch DB Load Task")
+def dispatch_load_news_data(data: pd.DataFrame, category: str):
+    data_size = len(data)
+    print(f"-> [Dispatcher] DataFrame received. Size: {data_size}")
+
+    if data.empty:
+        print("-> [Dispatcher] DataFrame is empty. Skipping DB load.")
+        return 0
+
+    print(f"-> [Dispatcher] Converting DataFrame of size {data_size} to list of records...")
+    
+    records = data.to_dict("records")
+    
+    print(f"-> [Dispatcher] Dispatching {len(records)} records for DB insertion via Celery.")
+    
+    result = insert_articles_task.apply(args=[records, category])
+    
+    articles_inserted = result.get()
+    
+    print(f"-> [Dispatcher] Celery task completed. {articles_inserted} articles inserted.")
+    
+    return articles_inserted
+
+
 @task
-def load_news_data(data: pd.DataFrame, category: str):
-    session = get_db() 
+def load_praw_data(data):
+    print(f"Loading {len(data)} PRAW records...")
+    return True
+
+
+@task
+def load_alpaca_data(data):
+    print(f"Loading {len(data)} Alpaca records...")
+    return True
+
+
+# ----------------------------------------
+# CELERY TASKS
+# ----------------------------------------
+@app.task(name="insert_articles_to_db")
+def insert_articles_task(records: List[Dict[str, Any]], category: str) -> int:
+    articles_count_received = len(records)
+    print(f"-> [Worker] Received {articles_count_received} records for insertion.")
+    
+    if articles_count_received == 0:
+        print("-> [Worker] Skipping DB insertion, 0 records received.")
+        return 0
+
+    session = next(get_db())
+    articles_count = 0
 
     try:
-        records = data.to_dict("records")
-        
         articles = []
         for record in records:
+            exists = (session
+                      .execute(select(Article).where(Article.url == record.get("url")))
+                      .scalars()
+                      .first())
+
+            if exists is not None:
+                continue
+
             article = Article(
                 author=record.get("author"),
                 title=record.get("title"),
@@ -25,26 +84,20 @@ def load_news_data(data: pd.DataFrame, category: str):
             )
             articles.append(article)
         
+        articles_count = len(articles)
+        
         # Bulk insert
         session.add_all(articles)
         session.commit()
         
-        print(f"Successfully loaded {len(articles)} articles to database")
-        return len(articles)
+        print(f"-> [Worker] Successfully loaded {articles_count} articles to database.")
+        return articles_count
         
     except Exception as e:
         session.rollback()
-        print(f"Error loading data: {e}")
+        print(f"-> [Worker] Error loading data: {e}")
+        # Re-raise the exception so Celery can handle retries if configured
         raise e
     finally:
         session.close()
 
-@task
-def load_praw_data(data):
-    print(f"Loading {len(data)} PRAW records...")
-    return True
-
-@task
-def load_alpaca_data(data):
-    print(f"Loading {len(data)} Alpaca records...")
-    return True
